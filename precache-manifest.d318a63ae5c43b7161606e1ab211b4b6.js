17010bb430683d58fc7a8d3ccba056f9731ac3a1
FaisalRizky <faisal.rizky@dansmultipro.com>
2019-11-04 20:49:25 +0700

    Updates

---
+++ precache-manifest.d318a63ae5c43b7161606e1ab211b4b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf02c4bad74fe3ff3389854c3b63c84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f950630c0fd5ae9b7180",
+    "url": "/static/css/2.699efa4f.chunk.css"
+  },
+  {
+    "revision": "64097629db2f94d1f9f7",
+    "url": "/static/css/main.fcf33877.chunk.css"
+  },
+  {
+    "revision": "f950630c0fd5ae9b7180",
+    "url": "/static/js/2.5c784f1f.chunk.js"
+  },
+  {
+    "revision": "64097629db2f94d1f9f7",
+    "url": "/static/js/main.696fd382.chunk.js"
+  },
+  {
+    "revision": "b3ea80565194df34d3a2",
+    "url": "/static/js/runtime-main.37ed9883.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/FaisalRConversion/precache-manifest.7a3d1a237d53cc15c8567e0faff53082.js"
+  "/precache-manifest.d318a63ae5c43b7161606e1ab211b4b6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/FaisalRConversion/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
